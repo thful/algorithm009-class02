9e3760f1e43777f13964afd681d8037f8675ec28
马铁蛋 <thful@thful.com>
2020-07-05 01:15:05 +0800

    0208实现Trie

---
+++ Week_07/_0208实现Trie.java
@@ -0,0 +1,103 @@
+package 字典树;
+
+/**
+ * 实现一个 Trie (前缀树)，包含 insert, search, 和 startsWith 这三个操作。
+ * <p>
+ * 示例:
+ * Trie trie = new Trie();
+ * trie.insert("apple");
+ * trie.search("apple");   // 返回 true
+ * trie.search("app");     // 返回 false
+ * trie.startsWith("app"); // 返回 true
+ * trie.insert("app");
+ * trie.search("app");     // 返回 true
+ * <p>
+ * 说明:
+ * 你可以假设所有的输入都是由小写字母 a-z 构成的。
+ * 保证所有输入均为非空字符串。
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/implement-trie-prefix-tree
+ */
+
+public class _0208实现Trie {
+    public static void main(String[] args) {
+        _0208实现Trie run = new _0208实现Trie();
+
+        run.insert("apple");
+        System.out.println(run.search("apple"));   // 返回 true
+        System.out.println(run.search("app"));     // 返回 false
+        System.out.println(run.startsWith("app")); // 返回 true
+        run.insert("app");
+        System.out.println(run.search("app"));     // 返回 true
+    }
+
+    private TrieNode root;
+
+    public _0208实现Trie() {
+        root = new TrieNode();
+    }
+
+    public void insert(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char currentChar = word.charAt(i);
+            if (!node.containsKey(currentChar)) {
+                node.put(currentChar, new TrieNode());
+            }
+            node = node.get(currentChar);
+        }
+        node.setEnd();
+    }
+
+    private TrieNode searchPrefix(String word) {
+        TrieNode node = root;
+        for (int i = 0; i < word.length(); i++) {
+            char curLetter = word.charAt(i);
+            if (node.containsKey(curLetter)) {
+                node = node.get(curLetter);
+            } else {
+                return null;
+            }
+        }
+        return node;
+    }
+
+    public boolean search(String word) {
+        TrieNode node = searchPrefix(word);
+        return node != null && node.isEnd();
+    }
+
+    public boolean startsWith(String prefix) {
+        TrieNode node = searchPrefix(prefix);
+        return node != null;
+    }
+}
+
+class TrieNode {
+    private TrieNode[] links;
+    private boolean isEnd;
+
+    public TrieNode() {
+        links = new TrieNode[26];
+    }
+
+    public boolean containsKey(char ch) {
+        return links[ch - 'a'] != null;
+    }
+
+    public TrieNode get(char ch) {
+        return links[ch - 'a'];
+    }
+
+    public void put(char ch, TrieNode node) {
+        links[ch - 'a'] = node;
+    }
+
+    public void setEnd() {
+        isEnd = true;
+    }
+
+    public boolean isEnd() {
+        return isEnd;
+    }
+}
