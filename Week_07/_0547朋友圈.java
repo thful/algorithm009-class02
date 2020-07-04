5088f1b8f825f7c605d89ec2997c4d9dfc798c38
马铁蛋 <thful@thful.com>
2020-07-05 01:34:04 +0800

    _0547朋友圈

---
+++ Week_07/_0547朋友圈.java
@@ -0,0 +1,156 @@
+package 并查集;
+
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Queue;
+
+/**
+ * 班上有 N 名学生。其中有些人是朋友，有些则不是。他们的友谊具有是传递性。如果已知 A 是 B 的朋友，B 是 C 的朋友，那么我们可以认为 A 也是 C 的朋友。所谓的朋友圈，是指所有朋友的集合。
+ * 给定一个 N * N 的矩阵 M，表示班级中学生之间的朋友关系。如果M[i][j] = 1，表示已知第 i 个和 j 个学生互为朋友关系，否则为不知道。你必须输出所有学生中的已知的朋友圈总数。
+ * <p>
+ * 示例 1:
+ * 输入:
+ * [[1,1,0],
+ * [1,1,0],
+ * [0,0,1]]
+ * 输出: 2
+ * 说明：已知学生0和学生1互为朋友，他们在一个朋友圈。
+ * 第2个学生自己在一个朋友圈。所以返回2。
+ * <p>
+ * 示例 2:
+ * 输入:
+ * [[1,1,0],
+ * [1,1,1],
+ * [0,1,1]]
+ * 输出: 1
+ * 说明：已知学生0和学生1互为朋友，学生1和学生2互为朋友，所以学生0和学生2也是朋友，所以他们三个在一个朋友圈，返回1。
+ * <p>
+ * 注意：
+ * N 在[1,200]的范围内。
+ * 对于所有学生，有M[i][i] = 1。
+ * 如果有M[i][j] = 1，则有M[j][i] = 1。
+ * <p>
+ * 链接：https://leetcode-cn.com/problems/friend-circles
+ */
+public class _0547朋友圈 {
+    // 深度优先搜索
+    public int findCircleNum(int[][] M) {
+        int[] visited = new int[M.length];
+        int count = 0;
+        for (int i = 0; i < M.length; i++) {
+            if (visited[i] == 0) {
+                dfs(M, visited, i);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public void dfs(int[][] M, int[] visited, int i) {
+        for (int j = 0; j < M.length; j++) {
+            if (M[i][j] == 1 && visited[j] == 0) {
+                visited[j] = 1;
+                dfs(M, visited, j);
+            }
+        }
+    }
+
+    // 广度优先遍历
+    public int findCircleNum2(int[][] M) {
+        int peopleCount = M.length;
+        int[] visited = new int[peopleCount];
+        int count = 0;
+        Queue<Integer> queue = new ArrayDeque<>();
+        for (int i = 0; i < peopleCount; i++) {
+            if (visited[i] == 0) {
+                queue.add(i);
+                while (!queue.isEmpty()) {
+                    int curPeople = queue.poll();
+                    visited[curPeople] = 1;
+                    for (int j = 0; j < peopleCount; j++) {
+                        if (M[curPeople][j] == 1 && visited[j] == 0) {
+                            queue.add(j);
+                        }
+                    }
+                }
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // 并查集搜索
+    public int findCircleNum3(int[][] M) {
+        int n = M.length;
+        UnionFind union = new UnionFind(n);
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j <= n; j++) {
+                if (i == j) continue;
+                if (M[i][j] == 1) union.union(i, j);
+            }
+        }
+        return union.count;
+    }
+
+
+    // 并查集搜索2
+    public int findCircleNum4(int[][] M) {
+        int[] parent = new int[M.length];
+        Arrays.fill(parent, -1);
+        for (int i = 0; i < M.length; i++) {
+            for (int j = 0; j < M.length; j++) {
+                if (M[i][j] == 1 && i != j) {
+                    union(parent, i, j);
+                }
+            }
+        }
+        int count = 0;
+        for (int i = 0; i < parent.length; i++) {
+            if (parent[i] == -1)
+                count++;
+        }
+        return count;
+    }
+
+    int find(int parent[], int i) {
+        if (parent[i] == -1)
+            return i;
+        return find(parent, parent[i]);
+    }
+
+    void union(int parent[], int x, int y) {
+        int xset = find(parent, x);
+        int yset = find(parent, y);
+        if (xset != yset)
+            parent[xset] = yset;
+    }
+}
+
+class UnionFind {
+    public int count = 0;
+    public int[] parent;
+
+    public UnionFind(int n) {
+        count = n;
+        parent = new int[n];
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    public int find(int p) {
+        while (p != parent[p]) {
+            parent[p] = parent[parent[p]];
+            p = parent[p];
+        }
+        return p;
+    }
+
+    public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ) return;
+        parent[rootP] = rootQ;
+        count--;
+    }
+}
